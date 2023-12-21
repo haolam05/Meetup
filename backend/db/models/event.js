22586ec7580424bf930baa01aeb3386235892409
Hao Lam <tuonghao2001@gmail.com>
2023-12-21 12:37:10 -0800

    Create EventImages Table

--- backend/db/models/event.js
+++ backend/db/models/event.js
@@ -10,7 +10,14 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Event.hasMany(
+        models.EventImage,
+        {
+          foreignKey: 'eventId',
+          hooks: true,
+          onDelete: 'cascade'
+        }
+      );
     }
   }
   Event.init({
