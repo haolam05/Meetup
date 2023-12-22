2d66cde6c5597feb68b442d67f5ace3f66e165b1
Hao Lam <tuonghao2001@gmail.com>
2023-12-22 11:10:42 -0800

    Add /api/groups/current backend endpoint

--- backend/controller/groupController.js
+++ backend/controller/groupController.js
@@ -1,20 +1,27 @@
 const { Group } = require('../db/models');
 
-async function getAllGroups(req, res) {
-  let groups = await Group.findAll();
+async function getAllGroups(_req, res) {
+  const groups = await Group.findAll();
+  res.json({ Groups: await _countNumMembersAndGetPreviewURL(groups) });
+}
 
+async function getAllGroupsOrganizedByCurrentUser(req, res) {
+  const groupsByCurrentUser = await req.user.getGroups();
+  res.json({ Groups: await _countNumMembersAndGetPreviewURL(groupsByCurrentUser) });
+}
+
+async function _countNumMembersAndGetPreviewURL(groups) {
   for (let i = 0; i < groups.length; i++) {
     const group = groups[i];
     const members = await group.getMembers();
     const previewImages = await group.getGroupImages({ where: { preview: true } });
-    console.log(previewImages.map(img => img.toJSON()));
     const previewImage = previewImages.length ? previewImages[0].url : 'Preview Image Not Found';
     groups[i] = { ...group.toJSON(), numMembers: members.length, previewImage };
   }
-
-  res.json({ Groups: groups });
+  return groups;
 }
 
 module.exports = {
-  getAllGroups
+  getAllGroups,
+  getAllGroupsOrganizedByCurrentUser
 }
--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const groupController = require('../../controller/groupController');
+const { requireAuth } = require('../../controller/authController');
+
+router.get('/current', requireAuth, groupController.getAllGroupsOrganizedByCurrentUser);
 
 router
   .route('/')
