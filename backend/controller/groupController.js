f4051c95421abfd5a5aac43b15cb84cc99b432b8
Hao Lam <tuonghao2001@gmail.com>
2023-12-22 11:21:45 -0800

    Add /api/groups/:groupId backedn endpoint

--- backend/controller/groupController.js
+++ backend/controller/groupController.js
@@ -1,15 +1,32 @@
-const { Group } = require('../db/models');
+const { Group, GroupImage, Venue } = require('../db/models');
 
-async function getAllGroups(_req, res) {
+async function getGroup(req, res) {
+  const group = await Group.findByPk(req.params.groupId, {
+    include: [
+      {
+        model: GroupImage
+      },
+      {
+        model: Venue,
+        through: {
+          attributes: []
+        }
+      }
+    ]
+  });
+  res.json(group);
+}
+
+async function getGroups(_req, res) {
   const groups = await Group.findAll();
   res.json({ Groups: await _countNumMembersAndGetPreviewURL(groups) });
 }
 
-async function getAllGroupsOrganizedByCurrentUser(req, res) {
+async function getGroupsOrganizedByCurrentUser(req, res) {
   const groupsByCurrentUser = await req.user.getGroups();
   res.json({ Groups: await _countNumMembersAndGetPreviewURL(groupsByCurrentUser) });
-}
 
+}
 async function _countNumMembersAndGetPreviewURL(groups) {
   for (let i = 0; i < groups.length; i++) {
     const group = groups[i];
@@ -22,6 +39,7 @@ async function _countNumMembersAndGetPreviewURL(groups) {
 }
 
 module.exports = {
-  getAllGroups,
-  getAllGroupsOrganizedByCurrentUser
+  getGroup,
+  getGroups,
+  getGroupsOrganizedByCurrentUser
 }
--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const groupController = require('../../controller/groupController');
 const { requireAuth } = require('../../controller/authController');
 
-router.get('/current', requireAuth, groupController.getAllGroupsOrganizedByCurrentUser);
+router.get('/current', requireAuth, groupController.getGroupsOrganizedByCurrentUser);
+router.get('/:groupId', groupController.getGroup);
 
 router
   .route('/')
-  .get(groupController.getAllGroups);
+  .get(groupController.getGroups);
 
 
 module.exports = router;
