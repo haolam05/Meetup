974ad42fd0a6ea796e83bacdbb9a67f3708f845a
Hao Lam <tuonghao2001@gmail.com>
2024-01-18 12:49:35 -0800

    initial setup

---
+++ frontend/src/main.jsx
@@ -0,0 +1,20 @@
+import { Provider } from 'react-redux';
+import configureStore from './store';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
