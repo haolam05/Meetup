09c5ce6690acd6c66df0685bae3e2994803d0902
Hao Lam <tuonghao2001@gmail.com>
2024-01-18 16:42:34 -0800

    Phase1: Login Form Page

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -4,24 +4,31 @@ const CREATE_SESSION = '/session/CREATE_SESSION';
 const DELETE_SESSION = '/session/DELETE_SESSION';
 
 // POJO action creators
-const createSession = user => ({ type: CREATE_SESSION, user });
-const deleteSession = () => ({ type: DELETE_SESSION, id });
+const createSession = payload => ({ type: CREATE_SESSION, payload });
+const deleteSession = () => ({ type: DELETE_SESSION });
 
 // Thunk action creators
-export const login = credentials => async dispatch => {
+export const login = credential => async dispatch => {
   const response = await csrfFetch(`/api/session`, {
     method: 'POST',
     body: JSON.stringify({
-      ...credentials
+      ...credential
     })
   });
 
-  if (response.ok) {
-    const data = await response.json();
-    dispatch(createSession(data));
-    return response;
-  }
+  const data = await response.json();
+  if (!response.ok) return { errors: data };
+  dispatch(createSession(data));
 };
+
+export const restoreSession = () => async dispatch => {
+  const response = await csrfFetch("/api/session");
+  const data = await response.json();
+  dispatch(createSession(data));
+};
+
+// Custom selectors
+export const sessionUser = state => state.session.user;
 
 // Reducer
 const initialState = { user: null };
@@ -29,7 +36,7 @@ const initialState = { user: null };
 function sessionReducer(state = initialState, action) {
   switch (action.type) {
     case CREATE_SESSION:
-      return { ...action.user };
+      return { ...action.payload };
     case DELETE_SESSION:
       return initialState;
     default:
