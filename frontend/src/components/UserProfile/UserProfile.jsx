313c8f31c768aa783a44be6e1ca7c461171894c3
Hao Lam <tuonghao2001@gmail.com>
2024-01-27 11:09:07 -0800

    added clickable links in user profile

--- frontend/src/components/UserProfile/UserProfile.jsx
+++ frontend/src/components/UserProfile/UserProfile.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { sortAscFuture, sortDescPast } from "../../utils/dateConverter";
 import Loading from "../Loading";
@@ -25,6 +25,7 @@ function UserProfile() {
   const hostedPastEvents = sortDescPast(hostedEvents);
   const attendedUpcomingEvents = sortAscFuture(attendedEvents);
   const attendedPastEvents = sortDescPast(attendedEvents);
+  const [ownedGroupsRef, joinedGroupsRef, hostedEventsRef, attendedEventsRef] = [useRef(null), useRef(null), useRef(null), useRef(null)];
 
   useEffect(() => {
     const loadUser = async () => {
@@ -67,10 +68,10 @@ function UserProfile() {
                 <div id="user-titles">
                   <div>Username</div>
                   <div>Email</div>
-                  <div>Owned</div>
-                  <div>Joined</div>
-                  <div>Hosted</div>
-                  <div>Attend</div>
+                  <div className="clickable" onClick={() => ownedGroupsRef.current.scrollIntoView()}>Owned</div>
+                  <div className="clickable" onClick={() => joinedGroupsRef.current.scrollIntoView()}>Joined</div>
+                  <div className="clickable" onClick={() => hostedEventsRef.current.scrollIntoView()}>Hosted</div>
+                  <div className="clickable" onClick={() => attendedEventsRef.current.scrollIntoView()}>Attend</div>
                 </div>
                 <div id="user-data">
                   <div>{user.username}</div>
@@ -96,19 +97,19 @@ function UserProfile() {
           </div>
         </div>
         <div id="user-details">
-          {ownedGroups.length ? <h2 className="subheading">Groups Owned ({ownedGroups.length})</h2> : ''}
+          {ownedGroups.length ? <h2 className="subheading" ref={ownedGroupsRef}>Groups Owned ({ownedGroups.length})</h2> : ''}
           {ownedGroups.length ? ownedGroups.map(group => <ManageGroup key={group.id} group={group} user={user} />) : ''}
 
-          {joinedGroups.length ? <h2 className="subheading">Groups Joined ({joinedGroups.length})</h2> : ''}
+          {joinedGroups.length ? <h2 className="subheading" ref={joinedGroupsRef}>Groups Joined ({joinedGroups.length})</h2> : ''}
           {joinedGroups.length ? joinedGroups.map(group => <ManageGroup key={group.id} group={group} user={user} />) : ''}
 
-          {hostedEvents.length ? <h2 className="subheading">Events Hosted ({hostedEvents.length})</h2> : ''}
+          {hostedEvents.length ? <h2 className="subheading" ref={hostedEventsRef}>Events Hosted ({hostedEvents.length})</h2> : ''}
           {hostedUpcomingEvents.length ? <h2 className="subheading gray">Upcoming Events ({hostedUpcomingEvents.length})</h2> : ''}
           {hostedUpcomingEvents.length ? hostedUpcomingEvents.map(event => <ManageEvent key={event.id} event={event} user={user} />) : ''}
           {hostedPastEvents.length ? <h2 className="subheading gray">Past Events ({hostedPastEvents.length})</h2> : ''}
           {hostedPastEvents.length ? hostedPastEvents.map(event => <ManageEvent key={event.id} event={event} user={user} />) : ''}
 
-          {attendedEvents.length ? <h2 className="subheading">Events Attended ({attendedEvents.length})</h2> : ''}
+          {attendedEvents.length ? <h2 className="subheading" ref={attendedEventsRef}>Events Attended ({attendedEvents.length})</h2> : ''}
           {attendedUpcomingEvents.length ? <h2 className="subheading gray">Upcoming Events ({attendedUpcomingEvents.length})</h2> : ''}
           {attendedUpcomingEvents.length ? attendedUpcomingEvents.map(event => <ManageEvent key={event.id} event={event} user={user} />) : ''}
           {attendedPastEvents.length ? <h2 className="subheading gray">Past Events ({attendedPastEvents.length})</h2> : ''}
