01420cbbc6102917a5daca4b225b7e4bb7d1dcc0
Hao Lam <tuonghao2001@gmail.com>
2024-01-27 15:47:07 -0800

    updated seed images to be hosted on aws

--- backend/db/seeders/20231222012410-demo-group-images.js
+++ backend/db/seeders/20231222012410-demo-group-images.js
@@ -26,23 +26,24 @@ const groupNames = [
   'Swimming'
 ];
 
+const location = 'https://meetup2024.s3.us-west-2.amazonaws.com/public/';
 const urls = [
-  'https://images.pexels.com/photos/10647765/pexels-photo-10647765.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/10350366/pexels-photo-10350366.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/10350338/pexels-photo-10350338.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/3165335/pexels-photo-3165335.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/194511/pexels-photo-194511.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/776654/pexels-photo-776654.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/207924/pexels-photo-207924.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/2507025/pexels-photo-2507025.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGh49ssIVPfbUnvZp74ryg-Km36tiaQ2QL0tAICM16_g5C6t4UwMxd9kwfjE68tdhJ3tk&usqp=CAU',
-  'https://news.virginia.edu/sites/default/files/Header_Soccer.jpg',
-  'https://villagrouploreto.s3.amazonaws.com/uploads/article/cover_en/391/what-golf-clubs-to-use-for-each-shot.jpg',
-  'https://cdn.britannica.com/73/244173-050-13235B84/Royal-Flush-poker-card-game-gambling.jpg',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvZJLsQ558cchr24kJP9K0VK1ceJsuMTT-BBGIRUREuWK3uavFtegwcA6i6PyXgHNGNBs&usqp=CAU',
-  'https://www.mdanderson.org/images/publications/focused-on-health/2019/web_healthy_cooking_1376x774.png.resize.702.404.jpg',
-  'https://img.etimg.com/thumb/width-1200,height-1200,imgsize-73554,resizemode-75,msid-103765538/top-trending-products/sports-equipment/best-badminton-racquets-to-unleash-your-potential-and-elevate-your-game.jpg',
-  'https://media.istockphoto.com/id/950300474/vector/children-swimming.jpg?s=612x612&w=0&k=20&c=4Cg3N65JBiZGHiqE3muh0_M17LHrCfOspTH_eyBvwAU='
+  location + 'volleyball.jpeg',
+  location + 'volleyball-2.jpeg',
+  location + 'volleyball-3.jpeg',
+  location + 'video-games.jpeg',
+  location + 'video-games-2.jpeg',
+  location + 'board-game.png',
+  location + 'board-game-2.jpeg',
+  location + 'theater.jpeg',
+  location + 'basketball.jpeg',
+  location + 'soccer.jpg',
+  location + 'golf.jpg',
+  location + 'poker.webp',
+  location + 'karaoke.jpeg',
+  location + 'cooking.jpg',
+  location + 'badminton.webp',
+  location + 'swimming.jpg'
 ];
 
 const groupImages = [
--- backend/db/seeders/20231222043052-demo-event-images.js
+++ backend/db/seeders/20231222043052-demo-event-images.js
@@ -30,31 +30,32 @@ const eventNames = [
   'Chopper Gang Reunion'
 ];
 
+const location = 'https://meetup2024.s3.us-west-2.amazonaws.com/public/';
 const urls = [
-  'https://wallpapers.com/images/featured-full/one-piece-pictures-bjm9tdff9yzguoup.jpg',
-  'https://wallpapers.com/images/high/one-piece-pictures-0ubzj7u8rx805ydb.webp',
-  'https://wallpapers.com/images/high/one-piece-pictures-22cqxo2cws8nh3hk.webp',
-  'https://cdn.vox-cdn.com/thumbor/QmYUW4WDPUe5cakWg1doB00HdBk=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19656977/5986912410_682fed19e2_b.jpg',
-  'https://wallpapers.com/images/high/monopoly-it-edition-hzkwq66wg7mwp9x5.webp',
-  'https://wallpapers.com/images/high/plastic-chess-black-white-nwjq6u60qsrp6dw1.webp',
-  'https://wallpapers.com/images/high/league-of-legends-dual-screen-3wyyss0c0w9f74ri.webp',
-  'https://wallpapers.com/images/high/league-of-legends-dual-screen-2576-x-1610-a6alrt217s8o81s6.webp',
-  'https://wallpapers.com/images/high/sport-beach-volleyball-athletes-hx7du1pwlxwatkkx.webp',
-  'https://wallpapers.com/images/high/beach-volleyball-men-and-women-silhouette-pkbic20kvawofcos.webp',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMbFYf3z8XZoon4QrAZciaEcXlgozQFXPqOkI8WPiDBsnpNeQoF7VGb5VaNuNt8xbVWRY&usqp=CAU',
-  'https://d3ieicw58ybon5.cloudfront.net/ex/450.450/u/27fbb0ec0344412dba96246436fe6d5e.jpg',
-  'https://i.guim.co.uk/img/media/7b6e9f51cd4758e7bb5b461b521756e54b192733/0_775_4670_5836/master/4670.jpg?width=700&quality=85&auto=format&fit=max&s=e53e05399def2f52287414766e7b46e7',
-  'https://imageio.forbes.com/specials-images/imageserve/5d7b1e04aea4d30008f0d674/0x0.jpg?format=jpg&height=900&width=1600&fit=bounds',
-  'https://media.istockphoto.com/id/937048674/vector/vector-illustration-of-kids-making-art-performance.jpg?s=612x612&w=0&k=20&c=sOhMafkCCvOSzMwrraGv1l2m_mtXiKnYK75Wk3wcEBE=',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFoxHcEk7X2TTgT0Ey69cZdDNQPz3RlHvvag&usqp=CAU',
-  'https://i0.wp.com/anitrendz.net/news/wp-content/uploads/2021/11/loveallplay_teasertrailer2screenshot.png?fit=1920%2C1080&ssl=1',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUxFwsTTQX2h86JHIp_gCFT6ZNUbiJU98EOQ&usqp=CAU',
-  'https://i.pinimg.com/originals/5b/df/d6/5bdfd6a360e0b56427d442015acbf9b8.gif',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-8x-FjPmUFCUisw4SIL6ExHGpZhnNqLDKLg&usqp=CAU',
-  'https://i.pinimg.com/originals/75/a5/67/75a5673957d77321e285a94efa076490.gif',
-  'https://gifdb.com/images/high/nami-cute-blush-nod-5tngxyjv2kmi1cyp.gif',
-  'https://i.pinimg.com/originals/aa/41/be/aa41bec777bf2a01bcf947a99c84181b.gif',
-  'https://i.pinimg.com/originals/26/1f/db/261fdbdd9daff46c9771c09e8592d6e2.gif'
+  location + 'mugiwara+pirate.jpg',
+  location + 'one+piece+festival.webp',
+  location + 'luffy+gear+5.webp',
+  location + 'mugiwara+priate+east+blue.jpg',
+  location + 'monopoly.webp',
+  location + 'chess.webp',
+  location + 'league+of+legends.webp',
+  location + 'league+of+legends2.webp',
+  location + 'volleyball.webp',
+  location + 'volleyball2.webp',
+  location + 'basketball-2.jpeg',
+  location + 'tsubasa.jpg',
+  location + 'golf2.webp',
+  location + 'poker-all-in.webp',
+  location + 'karaoker2.jpg',
+  location + 'baking2.jpeg',
+  location + 'badminton2.webp',
+  location + 'swimming2.jpeg	',
+  location + 'luffy-gear5-2.gif',
+  location + 'zoro.jpeg',
+  location + 'sanji.gif',
+  location + 'nami.gif',
+  location + 'robin.gif',
+  location + 'chopper.gif'
 ];
 
 const eventImages = [
