32fc6a87eb5786facc9f57c55da0b09478a189b1
Hao Lam <tuonghao2001@gmail.com>
2024-02-06 21:26:01 -0800

    bug fix getAverageLocation

--- frontend/src/utils/maps.js
+++ frontend/src/utils/maps.js
@@ -1,12 +1,13 @@
 export const getAverageLocation = locations => {
   const averageLocation = locations.reduce((acc, location) => {
-    acc.lat += location.lat;
-    acc.lng += location.lng;
+    acc.lat += parseFloat(location.lat);
+    acc.lng += parseFloat(location.lng);
     return acc;
   }, { lat: 0, lng: 0 });
-  averageLocation.lat = parseFloat(averageLocation.lat / locations.length);
-  averageLocation.lng = parseFloat(averageLocation.lng / locations.length);
-  console.log(averageLocation);
+
+  if (locations.length > 0) averageLocation.lat = averageLocation.lat / locations.length;
+  if (locations.length > 0) averageLocation.lng = averageLocation.lng / locations.length;
+
   return averageLocation;
 }
 
